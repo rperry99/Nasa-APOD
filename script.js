67c6183f7192149ffc5876645eb90517418ac37f
Russ <rperry1886@gmail.com>
2020-08-27 23:45:58 -0400

    Add main nav functionality

--- script.js
+++ script.js
@@ -12,6 +12,19 @@ const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${co
 let resultsArray = [];
 let favorites = {};
 
+// SHow Content
+function showContent(page) {
+  window.scrollTo({ top: 0, behavior: 'instant' });
+  if (page === 'results') {
+    resultsNav.classList.remove('hidden');
+    favoritesNav.classList.add('hidden');
+  } else {
+    resultsNav.classList.add('hidden');
+    favoritesNav.classList.remove('hidden');
+  }
+  loader.classList.add('hidden');
+}
+
 // Create DOM Nodes
 function createDOMNodes(page) {
   const currentArray =
@@ -91,14 +104,17 @@ function updateDOM(page) {
   }
   imagesContainer.textContent = '';
   createDOMNodes(page);
+  showContent(page);
 }
 
 // Get 10 images from NASA API
 async function getNasaPictures() {
+  // Show Loader
+  loader.classList.remove('hidden');
   try {
     const response = await fetch(apiUrl);
     resultsArray = await response.json();
-    updateDOM('favorites');
+    updateDOM('results');
   } catch (error) {
     // Catch Error Here
   }
